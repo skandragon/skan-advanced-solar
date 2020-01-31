643944ff24f4a79880af00c72df55e31545fb84c
flexoo <flexoo@users.noreply.github.com>
2020-01-31 16:36:37 +0000

    Update elite-solar.lua

--- prototypes/item/elite-solar.lua
+++ prototypes/item/elite-solar.lua
@@ -5,7 +5,7 @@ data:extend({
     icon = "__skan-advanced-solar__/graphics/elite-solar/elite-solar-icon.png",
     icon_size = 32,
     subgroup = "energy",
-    order = "d[solar-panel]-a[solar-panel]",
+    order = "d[solar-panel]-c[solar-panel]",
     place_result = "elite-solar",
     stack_size = 50,
     default_request_amount = 10
