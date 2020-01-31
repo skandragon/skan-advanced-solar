35c07c2521c727a7e0bf010d28d32b9018d13a51
flexoo <flexoo@users.noreply.github.com>
2020-01-31 16:37:26 +0000

    Update ultimate-solar.lua

--- prototypes/item/ultimate-solar.lua
+++ prototypes/item/ultimate-solar.lua
@@ -5,7 +5,7 @@ data:extend({
     icon = "__skan-advanced-solar__/graphics/ultimate-solar/ultimate-solar-icon.png",
     icon_size = 32,
     subgroup = "energy",
-    order = "d[solar-panel]-a[solar-panel]",
+    order = "d[solar-panel]-d[solar-panel]",
     place_result = "ultimate-solar",
     stack_size = 50,
     default_request_amount = 10
