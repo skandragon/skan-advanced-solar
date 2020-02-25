95fdf195834cf23144d6a74540916f87bd87be10
elenberg <everettlenberg@gmail.com>
2020-02-25 17:07:56 -0500

    Removing windows whitespace

--- prototypes/entity/accumulator.lua
+++ prototypes/entity/accumulator.lua
@@ -1,9 +1,9 @@
 local vanilla_entity = data.raw["accumulator"]["accumulator"]
 
 if not vanilla_entity.fast_replaceable_group then
-	vanilla_entity.fast_replaceable_group = "accumulator"
+    vanilla_entity.fast_replaceable_group = "accumulator"
 end
 
 if not vanilla_entity.next_upgrade then
-	vanilla_entity.next_upgrade = "advanced-accumulator"
+    vanilla_entity.next_upgrade = "advanced-accumulator"
 end
--- prototypes/entity/advanced-solar.lua
+++ prototypes/entity/advanced-solar.lua
@@ -1,29 +1,30 @@
 data:extend({
-  {
-    type = "solar-panel",
-    name = "advanced-solar",
-    icon = "__skan-advanced-solar__/graphics/advanced-solar/advanced-solar-icon.png",
-    icon_size = 32,
-    flags = {"placeable-neutral", "player-creation"},
-    minable = {hardness = 0.2, mining_time = 0.5, result = "advanced-solar"},
-    max_health = 200,
-    corpse = "big-remnants",
-    collision_box = {{-1.4, -1.4}, {1.4, 1.4}},
-    selection_box = {{-1.5, -1.5}, {1.5, 1.5}},
-	  fast_replaceable_group = "solar-panel",
-	  next_upgrade = "elite-solar",
-    energy_source =
     {
-      type = "electric",
-      usage_priority = "solar"
-    },
-    picture =
-    {
-      filename = "__skan-advanced-solar__/graphics/advanced-solar/advanced-solar.png",
-      priority = "high",
-      width = 104,
-      height = 96
-    },
-    production = "180kW"
-  }
-})
+        type = "solar-panel",
+        name = "advanced-solar",
+        icon = "__skan-advanced-solar__/graphics/advanced-solar/advanced-solar-icon.png",
+        icon_size = 32,
+        flags = {"placeable-neutral", "player-creation"},
+        minable = {hardness = 0.2, mining_time = 0.5, result = "advanced-solar"},
+        max_health = 200,
+        corpse = "big-remnants",
+        collision_box = {{-1.4, -1.4}, {1.4, 1.4}},
+        selection_box = {{-1.5, -1.5}, {1.5, 1.5}},
+        fast_replaceable_group = "solar-panel",
+        next_upgrade = "elite-solar",
+        energy_source =
+        {
+            type = "electric",
+            usage_priority = "solar"
+        },
+        picture =
+        {
+            filename = "__skan-advanced-solar__/graphics/advanced-solar/advanced-solar.png",
+            priority = "high",
+            width = 104,
+            height = 96
+        },
+        production = "180kW"
+    }})
+
+   
